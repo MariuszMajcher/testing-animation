751c7915ea73e13201721754d03c3e83b0ff844d
Mariusz Majcher <majchermx@gmail.com>
2024-05-30 17:23:58 +0100

    tried to create animation on elements generated with map, does not work, possibly becuase it regenerates new map

--- components/Block.tsx
+++ components/Block.tsx
@@ -5,11 +5,12 @@ import Link from 'next/link'
 export default function Page() {
 
     const [left, setLeft] = useState(false)
-
+    const characters : string[] = ["a", "b"]
     const clicked = () => {
         setLeft(!left)
     }
     return (
+        // NEXT THING TO TEST WILL BE TO CHECK IF THE ANIMATION HAPPENS ON OBJECTS THAT ARE GENERATED USING MAP, IF THAT DOES NOT WORK I CAN CREATE A COMPONENT THAT WILL HOLD EACH LETTER SEPARATELY
         <nav>
         <Link href="/" className={`square ${left ?  'left' : ''}`} onClick={clicked}
          style={{
@@ -18,7 +19,22 @@ export default function Page() {
 
         </Link>
          <Link href="/about" className={`square2 ${!left ?  'left' : ''}`} onClick={clicked}>
+         {characters.map((char: string, index) => {
+        const angle = left ? (100 / characters.length) * index + (120) : 0;
 
+        return (
+          <span
+            key={index}
+            className={`${"character"} ${left ? "active" : ''} ${!left ? "noncircular" : ''}`}
+            style={{
+              transform: left ? `rotate(${angle}deg) translate(${!left ? '1em' : '10em'}) rotate(${ -90  }deg)` : `rotate(${angle*1.5}deg) translate(${!left ?  '3em' : '12em'})`
+             
+            }}
+          >
+            {char}
+          </span>
+        );
+      })}
          </Link>
          </nav>
     );
